cb0b83fa565c3abfaf471a0bfa7766cc79f8a5a5
SentenzaX <francescomonorchio90@gmail.com>
2025-03-21 19:43:12 +0100

    Add files via upload

---
+++ Labirinto.java
@@ -0,0 +1,20 @@
+package it.uniroma3.diadia.ambienti;
+
+public class Labirinto {
+	private Stanza ingresso;
+	private Stanza uscita;
+	
+	public Stanza getIngresso() {
+		return ingresso;
+	}
+	public void setIngresso(Stanza ingresso) {
+		this.ingresso = ingresso;
+	}
+	public Stanza getUscita() {
+		return this.uscita=uscita;
+	}
+	public void setUscita(Stanza uscita) {
+		this.uscita = uscita;
+	}
+	
+}
